ef9a737f883e6b0e24d5cb745848a0f65b429e62
dustech <stefano.cerruti@outlook.com>
2024-05-19 17:25:25 +0200

    Add project properties

--- src/DusTech.CategoryTheory.UI/Program.cs
+++ src/DusTech.CategoryTheory.UI/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+#pragma warning disable CA1303
+using System;
+
 
 Console.WriteLine("Hello, World!");
+
+
+
+
+#pragma warning restore CA1303
